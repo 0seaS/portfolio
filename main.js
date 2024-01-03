34a9f305270a0192c6eed367ccd2fa4dfa589c5f
0seaS <00sea55@gmail.com>
2024-01-03 15:56:31 -0400

    potafolio terminado

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    
-  </div>
-`
+const darkMode = document.querySelector('#app')
+darkMode.innerHTML = `
+  <i class='bx bxs-moon' ></i>
+  `
+let isDark = false;
+
+const body = document.querySelector('#myBody')
+
+darkMode.addEventListener("click", () => {
+  if (isDark) {
+    darkMode.innerHTML = `
+    <i class='bx bxs-moon' ></i>
+    `
+    isDark = false;
+  } else {
+    darkMode.innerHTML = `
+    <i class='bx bxs-sun' ></i>
+    `;
+    isDark = true;
+  }
+  body.classList.toggle("darkMode")
+})
